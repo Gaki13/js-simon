e75e8890b6fdf5c48116ad4606972331d9de6354
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-05-15 15:53:03 +0200

    dopo che faccio vedere i numeri per 30 secondi gli chiedo all'utente 1 per 1

--- js/main.js
+++ js/main.js
@@ -12,11 +12,29 @@ function getRndInteger(min, max) {
 /*-------------------------
     MAIN
 --------------------------*/
+const numbersHtml = document.querySelector(".numbers");
+const timer = 3; // 3 secondi
 const numbersRandom = [];
 // genero 5 numeri casuali e li inserisco nell'array numbersRandom
 while(numbersRandom.length < 5) {
     const number = getRndInteger(1, 100);
     if(!numbersRandom.includes(number)) {
         numbersRandom.push(number);
+    }
+}
+
+// Visualizzare in pagina 5 numeri casuali.
+numbersHtml.innerHTML = numbersRandom;
+// nascondere i numeri
+setTimeout(function() {
+    numbersHtml.innerHTML = "";
+}, (timer - 1) * 1000); // 2000
+
+// Dopo 30 secondi l'utente deve inserire, i numeri che ha visto precedentemente, tramite il prompt().
+setTimeout(function() {
+    const userNumbers = [];
+    for(let i = 0; i < 5; i++) {
+        const number = Number(prompt("Inserisci un numero che hai visto in precendenza"));
+        userNumbers.push(number);
     }
 }
